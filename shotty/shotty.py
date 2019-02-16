5a73f5dd105ead144efd61d179b0695586cc0f36
unknown <trade2308@outlook.com>
2019-02-16 16:27:17 +0800

    added pep8 format style

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -5,34 +5,39 @@ import click
 session = boto3.Session(profile_name='shotty')
 ec2 = session.resource('ec2')
 
+
 def filter_instances(project):
     instances = []
 
     if project:
-        filters = [{'Name':'tag:Project', 'Values':[project]}]
+        filters = [{'Name': 'tag:Project', 'Values': [project]}]
         instances = ec2.instances.filter(Filters=filters)
     else:
         instances = ec2.instances.all()
 
     return instances
 
+
 def has_pending_snapshot(volume):
     snapshots = list(volume.snapshots.all())
     return snapshots and snapshots[0].state == 'pending'
+
 
 @click.group()
 def cli():
     """Shotty manages snapshots"""
 
+
 @cli.group('snapshots')
 def snapshots():
     """Commands for snapshots"""
 
+
 @snapshots.command('list')
 @click.option('--project', default=None,
-    help="only instances for project (tag Project:<name>)")
+              help="only instances for project (tag Project:<name>)")
 @click.option('--all', 'list_all', default=False, is_flag=True,
-    help="List all snapshots for each volume, not just the most recent")
+              help="List all snapshots for each volume, not just the most recent")
 def list_snapshots(project, list_all):
     "List EC2 instances volumes snapshots"
 
@@ -50,18 +55,20 @@ def list_snapshots(project, list_all):
                     s.start_time.strftime("%c")
                 )))
 
-                if s.state == 'completed' and not list_all: break
+                if s.state == 'completed' and not list_all:
+                    break
 
     return
+
 
 @cli.group('volumes')
 def volumes():
     """Commands for volumes"""
 
+
 @volumes.command('list')
 @click.option('--project', default=None,
-    help="only instances for project (tag Project:<name>)")
-
+              help="only instances for project (tag Project:<name>)")
 def list_volumes(project):
     "List EC2 instances volumes"
 
@@ -79,14 +86,15 @@ def list_volumes(project):
 
     return
 
+
 @cli.group('instances')
 def instances():
     """Commands for instances"""
 
 @instances.command('snapshot',
-    help="Create snapshots of all volumes")
+                   help="Create snapshots of all volumes")
 @click.option('--project', default=None,
-    help="only instances for project (tag Project:<name>)")
+              help="only instances for project (tag Project:<name>)")
 def create_snapshots(project):
     "Create snapshots for EC2 instances"
 
@@ -100,7 +108,8 @@ def create_snapshots(project):
 
         for v in i.volumes.all():
             if has_pending_snapshot(v):
-                print("   Skipping {0}, snapshot already in progress".format(v.id))
+                print(
+                    "   Skipping {0}, snapshot already in progress".format(v.id))
                 continue
 
             print("  Creating snapshot of {0}".format(v.id))
@@ -116,32 +125,30 @@ def create_snapshots(project):
     return
 
 
-
 @instances.command('list')
 @click.option('--project', default=None,
-    help="only instances for project (tag Project:<name>)")
-
+              help="only instances for project (tag Project:<name>)")
 def list_instances(project):
     "List EC2 instances"
     instances = filter_instances(project)
 
     for i in instances:
-        tags = { t['Key']: t['Value'] for t in i.tags or []}
-        print (', '.join((
-        i.id,
-        i.instance_type,
-        i.placement['AvailabilityZone'],
-        i.state['Name'],
-        i.public_dns_name,
-        tags.get('Project', '<no project>')
+        tags = {t['Key']: t['Value'] for t in i.tags or []}
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name,
+            tags.get('Project', '<no project>')
         )))
 
     return
 
+
 @instances.command('stop')
 @click.option('--project', default=None,
-    help='Only instances for project')
-
+              help='Only instances for project')
 def stop_instances(project):
     "Stop EC2 instances"
     instances = filter_instances(project)
@@ -156,10 +163,10 @@ def stop_instances(project):
             continue
     return
 
+
 @instances.command('start')
 @click.option('--project', default=None,
-    help='Only instances for project')
-
+              help='Only instances for project')
 def start_instances(project):
     "Start EC2 instances"
     instances = filter_instances(project)
@@ -173,6 +180,7 @@ def start_instances(project):
             continue
 
     return
+
 
 if __name__ == '__main__':
     cli()
